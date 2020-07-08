d44bb49645c1aa62813428c564928d59428a2511
ShivashishY <shivashishmca@gmail.com>
2020-07-08 14:43:40 +0530

    added download links of all apps

--- js/bounce.js
+++
@@ -1,8 +0,0 @@
-$(window).on("scroll", function() {
-    if($(window).scrollTop()) {
-        $('.header-section').addClass('black');
-    }
-    else {
-        $('.header-section').removeClass('black');
-    }
-})
